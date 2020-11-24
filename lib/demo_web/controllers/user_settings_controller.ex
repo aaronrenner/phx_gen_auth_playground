c5e72abd6b38deeaeb315f87b1fbb2e12e37bf9d
Aaron Renner <aaron.renner@gmail.com>
2020-11-23 21:53:07 -0700

    Consolidate update password and email actions

--- lib/demo_web/controllers/user_settings_controller.ex
+++ lib/demo_web/controllers/user_settings_controller.ex
@@ -10,7 +10,11 @@ defmodule DemoWeb.UserSettingsController do
     render(conn, "edit.html")
   end
 
-  def update_email(conn, %{"current_password" => password, "user" => user_params}) do
+  def update(conn, %{
+        "action" => "update_email",
+        "current_password" => password,
+        "user" => user_params
+      }) do
     user = conn.assigns.current_user
 
     case Accounts.apply_user_email(user, password, user_params) do
@@ -33,21 +37,11 @@ defmodule DemoWeb.UserSettingsController do
     end
   end
 
-  def confirm_email(conn, %{"token" => token}) do
-    case Accounts.update_user_email(conn.assigns.current_user, token) do
-      :ok ->
-        conn
-        |> put_flash(:info, "Email changed successfully.")
-        |> redirect(to: Routes.user_settings_path(conn, :edit))
-
-      :error ->
-        conn
-        |> put_flash(:error, "Email change link is invalid or it has expired.")
-        |> redirect(to: Routes.user_settings_path(conn, :edit))
-    end
-  end
-
-  def update_password(conn, %{"current_password" => password, "user" => user_params}) do
+  def update(conn, %{
+        "action" => "update_password",
+        "current_password" => password,
+        "user" => user_params
+      }) do
     user = conn.assigns.current_user
 
     case Accounts.update_user_password(user, password, user_params) do
@@ -59,6 +53,20 @@ defmodule DemoWeb.UserSettingsController do
 
       {:error, changeset} ->
         render(conn, "edit.html", password_changeset: changeset)
+    end
+  end
+
+  def confirm_email(conn, %{"token" => token}) do
+    case Accounts.update_user_email(conn.assigns.current_user, token) do
+      :ok ->
+        conn
+        |> put_flash(:info, "Email changed successfully.")
+        |> redirect(to: Routes.user_settings_path(conn, :edit))
+
+      :error ->
+        conn
+        |> put_flash(:error, "Email change link is invalid or it has expired.")
+        |> redirect(to: Routes.user_settings_path(conn, :edit))
     end
   end
 
